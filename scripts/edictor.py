6e56fde166a5eb2ca88e7085be142ffdda2e5418
Frederic Blum <frederic.blum@hu-berlin.de>
2021-02-13 21:22:53 +0100

    CU cogid and preliminary alignments

--- scripts/edictor.py
+++ scripts/edictor.py
@@ -1,7 +1,13 @@
 from lingpy import *
 
-wl = Wordlist.from_cldf('cldf/cldf-metadata.json')
-wl.add_entries('cogid', 'tokens', lambda x: 0)
-wl.add_entries('cogids', 'tokens', lambda x: 0)
-wl.add_entries('morphemes', 'tokens', lambda x: '?')
-wl.output('tsv', filename='wordlist')
+## Creating a worldlist for Edictor
+#wl = Wordlist.from_cldf('cldf/cldf-metadata.json')
+#wl.add_entries('cogid', 'tokens', lambda x: 0)
+#wl.add_entries('cogids', 'tokens', lambda x: 0)
+#wl.add_entries('morphemes', 'tokens', lambda x: '?')
+#wl.output('tsv', filename='wordlist')
+
+wl = Wordlist('wordlist.tsv')
+
+from lingpy.convert.strings import write_nexus
+nexus = write_nexus(wl, ref='ALIGNMENT', mode='beast', filename='chibcu2.nex')
