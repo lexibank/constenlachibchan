bc93b475fb8f0e90f0bf004886c3844ef974c74a
Tarotis <frederic.blum@hu-berlin.de>
2021-04-18 01:16:13 +0200

    nexus files for cu-data

--- scripts/edictor.py
+++ scripts/edictor.py
@@ -1,6 +1,9 @@
 from lexibank_constenlachibchan import Dataset
 from lingpy import *
 from lingpy.compare.partial import *
+from lingpy.convert.strings import write_nexus
+import io
+from lingpy.convert.strings import matrix2dst
 
 
 cols = ['concept_id', 'concept_name', 'language_id', 'language_name', 'value',
@@ -9,7 +12,7 @@ cols = ['concept_id', 'concept_name', 'language_id', 'language_name', 'value',
 
 
 DS = Dataset()
-lex = Partial.from_cldf(DS.cldf_dir.joinpath('cldf-metadata.json'),
+lex = Partial.from_cldf(DS.cldf_dir.joinpath('../cldf/cldf-metadata.json'),
         columns=cols)
 
 lex.partial_cluster(method='sca', threshold=0.45, ref="cogids")
@@ -18,15 +21,21 @@ alms.align()
 
 alms.add_entries('morphemes', 'tokens', lambda x: "")
 
-alms.output('tsv', filename='./scripts/AutoCognates/wordlist')
-alms = Wordlist('./scripts/AutoCognates/wordlist.tsv')
+at = ['Atanque'] 
+alms.output('tsv', 
+                filename='./AutoCognates/wordlist',
+                subset=True,
+                rows=dict(doculect = 'not in' +str(at)))
+alms = Wordlist('./AutoCognates/wordlist.tsv')
 
-# nexus = write_nexus(wl,
-#       ref="COGIDS", 
-#       mode="beast", 
-#       filename='./AutoCognates/chibchan-beast_cu.nex')
 
-# dst = matrix2dst(wl.get_distances(ref='COGID', mode='swadesh'), wl.taxa)
+nexus = write_nexus(alms,
+       ref="COGID", 
+       mode="beast", 
+       filename='./AutoCognates/chibchan-beast_cu.nex')
 
-# with io.open('./AutoCognates/chibchan_cu.dst', 'w', encoding='utf8') as fp:
-#    fp.write(dst)
+dst = matrix2dst(alms.get_distances(ref='COGID', mode='swadesh'), alms.taxa)
+
+
+with io.open('./AutoCognates/chibchan_cu.dst', 'w', encoding='utf8') as fp:
+    fp.write(dst)
